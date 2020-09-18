4d63df708e03a398da3ac72974d404bd04ddd471
Charles - the Coding Nyctophile <saitsong@yahoo.com>
2020-09-19 01:41:02 +0800

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'login.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Charles App',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
---
+++ myHomePage.dart
@@ -0,0 +1,52 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:tutorial_7/database.dart';
+import 'post.dart';
+import 'postList.dart';
+import 'textInputWidget.dart';
+
+class MyHomePage extends StatefulWidget {
+  final FirebaseUser user;
+
+  MyHomePage(this.user);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Post> posts = [];
+
+  void newPost(String text) {
+    var post = new Post(text, widget.user.displayName);
+    savePost(post);
+    post.setId(savePost(post));
+    this.setState(() {
+      posts.add(post);
+    });
+  }
+
+  void updatePosts() {
+    getAllPosts().then((posts) => {
+          this.setState(() {
+            this.posts = posts;
+          })
+        });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    updatePosts();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("Hello Charles App")),
+        body: Column(children: <Widget>[
+          Expanded(child: PostList(this.posts, widget.user)),
+          TextInputWidget(this.newPost)
+        ]));
+  }
+}
